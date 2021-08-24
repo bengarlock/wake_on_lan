16a085288cc2aea401e5bd28d848239f920ec680
bengarlock <ben.garlock@gmail.com>
2021-08-24 07:02:18 -0400

    added systems function

--- wake_on_lan.py
+++ wake_on_lan.py
@@ -4,8 +4,23 @@ import systems
 
 systems = systems.return_systems()
 
+root = Tk()
+root.title("Wake On Lan")
+title_row = Label(root, width=40, text="Wake On Lan Tool")
+title_row.grid(row=0, column=0, columnspan=2)
 
-print(systems)
+console = Label(root, width=40, text='Select a system to wake')
+console.grid(row=2, column=1)
 
 
+def wake_up(system):
+    send_magic_packet(system['mac_address'])
+    display_console = Label(root, width=40, text=f"Wakeing up {system['name']} - {system['mac_address']}")
+    display_console.grid(row=2, column=1, rowspan=2)
 
+
+for system in systems:
+    button = Button(root, text=system['name'], padx=60, pady=20, border=10, command=lambda: wake_up(system))
+    button.grid(row=systems.index(system) + 2, column=0)
+
+root.mainloop()
