0d34300a4492bf2d9a92855bfc66a354083d4ad7
bengarlock <ben.garlock@gmail.com>
2021-08-24 07:03:49 -0400

    checking...

--- systems.py
+++ systems.py
@@ -1,5 +1,5 @@
 def return_systems():
-    systems = []
-    bluescreen = {'name': 'Bluescreen', 'mac_address': 'D4-5D-64-D4-91-41'}
-    systems.append(bluescreen)
+    systems = [
+        {'name': 'Bluescreen', 'mac_address': 'D4-5D-64-D4-91-41'}
+    ]
     return systems
